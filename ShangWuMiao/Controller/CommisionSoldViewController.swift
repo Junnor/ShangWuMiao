fc6a65a6b7354b7afd6b8f7c8706e278cbecbf8d
ZhuDengquan <dengquanzhu@gmail.com>
2017-05-22 18:42:49 +0800

    添加网页代理

--- ShangWuMiao/Controller/CommisionSoldViewController.swift
+++ ShangWuMiao/Controller/CommisionSoldViewController.swift
@@ -6,9 +6,32 @@
 //  Copyright © 2017年 moelove. All rights reserved.
 //
 
+private let delegateUrlString = "https://www.nyato.com/help/annouce_detail/63?apppage=1"
+
 import UIKit
+import SVProgressHUD
 
-class CommisionSoldViewController: UIViewController {
+class CommisionSoldViewController: UIViewController, UIWebViewDelegate {
 
-    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var webView: UIWebView! {
+        didSet {
+            webView.delegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Nyato喵特网 - 有爱、贴心、便捷的漫展服务平台 -"
+        
+        let url = URL(string: delegateUrlString)
+        let reuqest = URLRequest(url: url!)
+        webView.loadRequest(reuqest)
+    }
+    
+    // MARK: - Delegate
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        SVProgressHUD.showError(withStatus: "请求失败，请重新加载")
+    }
+    
 }
