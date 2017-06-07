e817d1cd0729188a150fd9634a8fe18e4a1cc539
Dengquan Zhu <dengquanzhu@gmail.com>
2017-06-07 16:30:27 +0800

    集成Apple Pay

--- ShangWuMiao/Controller/TopupViewController.swift
+++ ShangWuMiao/Controller/TopupViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import SVProgressHUD
 import SwiftyJSON
 import PassKit
+import Alamofire
 
 class TopupViewController: UIViewController {
     
@@ -33,24 +34,32 @@ class TopupViewController: UIViewController {
         }
     }
     
-    fileprivate let numberOfChoice = 2
+    fileprivate var numberOfChoice = 1
     fileprivate let topupIdentifier = "top up identifier"
     fileprivate let applePayIdentifier = "Apple Pay"
+    
+    // For Apple pay
+    fileprivate var tn: String!
+    // "00" for distrubution, "01" for testing
+    fileprivate let mode = "01"     // TODO: replace
+    fileprivate let nyatoMerchantID = "merchant.com.example.merchantname.ts"    // TODO: replace
+    fileprivate var payNetworks = [PKPaymentNetwork]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "充值喵币"
+        numberOfChoice = isApplePayAvailable() ? 2 : 1
 
+        title = "充值喵币"
         mcoinsLabel?.text = "\(User.shared.mcoins)"
-        
-        
         NotificationCenter.default.addObserver(self, selector: #selector(refreshMcoins), name: nyatoMcoinsChange, object: nil)
     }
     
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
+    
     
     @objc private func refreshMcoins() {
         mcoinsLabel?.text = "\(User.shared.mcoins)"
@@ -96,18 +105,20 @@ extension TopupViewController: UITableViewDataSource, UITableViewDelegate {
             cell.titleLabel?.text = "使用支付宝支付"
             
         } else if let cell = cell as? ApplePayCell {
+            for subview in cell.applyPayView.subviews {
+                subview.removeFromSuperview()
+            }
             if #available(iOS 8.3, *) {
-                for subview in cell.applyPayView.subviews {
-                    subview.removeFromSuperview()
-                }
                 let payButton = PKPaymentButton(type: .plain, style: .white)
                 payButton.frame = cell.applyPayView.bounds
+                
                 cell.applyPayView.backgroundColor = UIColor.white
                 cell.applyPayView.addSubview(payButton)
                 cell.applyPayView.clipsToBounds = true
             } else {
                 // Fallback on earlier versions
             }
+            
             cell.titleLabel.text = "Apple Pay"
         }
         return cell
@@ -126,35 +137,133 @@ extension TopupViewController: UITableViewDataSource, UITableViewDelegate {
                             }
             })
         } else if indexPath.row == 1 { // Apple pay
-            // pay
-            
+            applePay()
         } else {  // wechat
-            
-            // TODO: Hidden Hidden Hidden Hidden Hidden Hidden Hidden
             /*
-             isWechat = true
-             // Float(currentMcoinsCount)
-             UserPay.pay(withType: Pay.wechat,
-             orderPrice: Float(1),
-             completionHandler: { [weak self] (success, info) in
-             if success {
-             print("pay.....")
-             self?.wechatAction()
-             } else {
-             SVProgressHUD.showError(withStatus: info!)
-             print("wehcat pay failure: \(info!)")
-             }
-             
-             })
+            // TODO: Hidden Hidden Hidden Hidden Hidden Hidden Hidden
+            isWechat = true
+            // Float(currentMcoinsCount)
+            UserPay.pay(withType: Pay.wechat,
+                        orderPrice: Float(1),
+                        completionHandler: { [weak self] (success, info) in
+                            if success {
+                                print("pay.....")
+                                self?.wechatAction()
+                            } else {
+                                SVProgressHUD.showError(withStatus: info!)
+                                print("wehcat pay failure: \(info!)")
+                            }
+                            
+            })
              */
         }
     }
 
 }
 
-// Apple Pay
+// MARK: - Apple Pay
 
-fileprivate extension TopupViewController {
+extension TopupViewController: UPAPayPluginDelegate {
+    
+    // MARK: UPAPay Delegate
+    func upaPayPluginResult(_ payResult: UPPayResult!) {
+        if let status = payResult?.paymentResultStatus {
+            switch status {
+            case .success:
+                print("success")
+                let otherInfo = payResult.otherInfo ?? ""
+                let successInfo = "支付成功\n\(otherInfo)"
+                showAlert(successInfo)
+            case .failure:
+                print("failure")
+                let errorInfo = payResult.errorDescription ?? "支付失败"
+                showAlert(errorInfo)
+            case .cancel:
+                print("cancel")
+                showAlert("支付取消")
+            case .unknownCancel:
+                print("unknownCancel")
+                let errorInfo = ""
+                // TODO: get [errorInfo] from server, may success or failure
+                showAlert(errorInfo)
+            }
+        }
+    }
+
+    // MARK: - Helper
+    
+    fileprivate func isApplePayAvailable() -> Bool {
+        var available = false
+        
+        // 需要银联
+        if #available(iOS 9.2, *) {
+            if PKPaymentAuthorizationViewController.canMakePayments() {
+                payNetworks = [.chinaUnionPay]
+                available = true
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+
+        return available
+    }
+    
+    
+    @objc fileprivate func applePay() {
+        // Check whether the network is support
+        if !PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: payNetworks) {
+            let msg = "当前设备没有包含支持的支付银联卡, 你可以到 Wallet 应用添加银联卡"
+            showAlert(msg)
+            return
+        }
+        
+        // Get TN
+        fetchTransactionNumber { [weak self] tnResult in
+            if tnResult != nil {
+                self?.tn = tnResult
+                self?.tnPay()
+            }
+        }
+    }
+    
+    private func tnPay() {
+        if tn != nil && tn.characters.count > 0 {
+            UPAPayPlugin.startPay(tn,
+                                  mode: mode,
+                                  viewController: self,
+                                  delegate: self,
+                                  andAPMechantID: nyatoMerchantID)
+        } else {
+            showAlert("获得交易单号失败")
+        }
+    }
+
+    private func fetchTransactionNumber(callbacK: @escaping (_ tn: String?) -> ()) {
+        // TODO: replace
+        if let url = URL(string: "http://101.231.204.84:8091/sim/getacptn") {
+            Alamofire.request(url,
+                              method: .post,
+                              parameters: nil,
+                              encoding: URLEncoding.default,
+                              headers: nil).responseJSON { response in
+                                switch response.result {
+                                case .success(let data):
+                                    let str = String(describing: data)
+                                    callbacK(str)
+                                case .failure(let error):
+                                    print("Fetch TN error: \(error)")
+                                }
+                                
+            }
+        }
+    }
+    
+    private func showAlert(_ info: String) {
+        let alert = UIAlertController(title: "提示", message: info, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "确定", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 // MARK: - Alipay
